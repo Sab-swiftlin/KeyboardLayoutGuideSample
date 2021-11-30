d2cbbca69321daea2835c266e4b8c5567288d5bf
sab_swiftlin <nukunuku23@gmail.com>
2021-11-30 22:44:38 +0900

    Added BasicView

---
+++ KeyboardLayoutGuideSample/KeyboardLayoutGuideSample/Basic/BasicViewController.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class BasicViewController: UIViewController {
+    private lazy var field: UITextField = {
+        let field = UITextField()
+        field.translatesAutoresizingMaskIntoConstraints = false
+        field.borderStyle = .roundedRect
+        return field
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpUI()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    private func setUpUI() {
+        view.addSubview(field)
+        NSLayoutConstraint.activate([
+            field.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            field.widthAnchor.constraint(equalToConstant: 200),
+            field.heightAnchor.constraint(equalToConstant: 40),
+            view.keyboardLayoutGuide.topAnchor.constraint(equalTo: field.bottomAnchor, constant: 10)
+        ])
+    }
+}
