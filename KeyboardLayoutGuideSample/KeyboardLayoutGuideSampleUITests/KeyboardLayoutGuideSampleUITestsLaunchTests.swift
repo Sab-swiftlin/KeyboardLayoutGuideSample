a58b38eca83d10d93efe3bbcb00042e2d37bff33
Sab-swiftlin <50907353+Sab-swiftlin@users.noreply.github.com>
2021-11-30 22:14:17 +0900

    Added a project file. (#1)

---
+++ KeyboardLayoutGuideSample/KeyboardLayoutGuideSampleUITests/KeyboardLayoutGuideSampleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  KeyboardLayoutGuideSampleUITestsLaunchTests.swift
+//  KeyboardLayoutGuideSampleUITests
+//
+//  Created by Haruki Saburi on 2021/11/30.
+//
+
+import XCTest
+
+class KeyboardLayoutGuideSampleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
