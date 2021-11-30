936508de3cc5b4033c91c17858a1a5e413d45843
sab_swiftlin <nukunuku23@gmail.com>
2021-11-30 22:11:30 +0900

    Added a project file.

---
+++ KeyboardLayoutGuideSample/KeyboardLayoutGuideSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KeyboardLayoutGuideSample
+//
+//  Created by Haruki Saburi on 2021/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
